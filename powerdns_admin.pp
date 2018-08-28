95fc84aca1fb16d580b24f1471e3c0e6ac73737c
Maximilian Stinsky <maximilian@stinsky.com>
2018-08-28 19:12:27 +0200

    Dont setup ssl

--- powerdns_admin.pp
+++ powerdns_admin.pp
@@ -18,26 +18,11 @@ nginx::resource::server { 'poweradmin':
   proxy_set_header     => ['Host $host', 'X-Real-IP $remote_addr', 'X-Forwarded-For $proxy_add_x_forwarded_for'],
   client_max_body_size => '10m',
   ssl                  => true,
-  listen_port          => 443,
-  ssl_cert             => '/etc/nginx/certs/cert.pem',
-  ssl_key              => '/etc/nginx/certs/key.pem',
-  ssl_ciphers          => 'EECDH+AESGCM:EDH+AESGCM:EECDH:EDH:!MD5:!RC4:!LOW:!MEDIUM:!CAMELLIA:!ECDSA:!DES:!DSS:!3DES:!NULL',
-  ssl_protocols        => 'TLSv1.1 TLSv1.2',
-}
-
-nginx::resource::server { 'poweradmin_ssl_redirect':
-  ensure              => present,
-  server_name         => ['changeme_domain_name'],
-  listen_port         => 80,
-  location_cfg_append => {
-    rewrite             => "^ https://changeme_domain_name\$request_uri? permanent",
-  },
+  listen_port          => 80,
 }
 
 nginx::resource::location { 'poweradmin_root':
   ensure                => present,
-  ssl                   => true,
-  ssl_only              => true,
   server                => 'poweradmin',
   location              => '/',
   proxy                 => 'http://unix:/opt/web/powerdns-admin/powerdns-admin.sock',
@@ -48,8 +33,6 @@ nginx::resource::location { 'poweradmin_root':
 
 nginx::resource::location { 'poweradmin_static':
   ensure   => present,
-  ssl      => true,
-  ssl_only => true,
   server   => 'poweradmin',
   location => '~ ^/static/',
   www_root => '/opt/web/powerdns-admin/app',
